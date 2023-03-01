a34a6c8e5eb9901c5c2d170fa13ba44725759c85
Taras Kachmar <kachmar.taras@gmail.com>
2023-03-01 02:16:54 +0200

    done task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]')
+}
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+let activeStatusBtn = refs.stopBtn.hasAttribute('disabled');
+
+if(!activeStatusBtn){
+    onStopBtnAttribute();
+}
+
+function onStartBtnClick(){
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    refs.stopBtn.removeAttribute('disabled');
+    refs.startBtn.setAttribute('disabled', '');
+}
+
+function onStopBtnClick(){
+    clearInterval(intervalId);
+
+    refs.startBtn.removeAttribute('disabled');
+    onStopBtnAttribute();
+}
+
+function onStopBtnAttribute(){
+    refs.stopBtn.setAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
