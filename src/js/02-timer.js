a5ee13cd2a5de131c6f742b46ff86ee4ef4739ce
Taras Kachmar <kachmar.taras@gmail.com>
2023-03-04 03:59:41 +0200

    test

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
-
+let startTime;
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -9,7 +9,6 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const selectTime = selectedDates[0] - options.defaultDate;
-    let startTime;
     if(selectTime < 0){
       // alert("Please choose a date in the future");
       Notiflix.Notify.failure("Please choose a date in the future");
@@ -20,9 +19,7 @@ const options = {
     startTime = selectedDates[0].getTime();
   }
 };
-  
 flatpickr("#datetime-picker", options);
-
 const refs = {
   inputEl: document.getElementById('datetime-picker'),
   startBtnEl: document.querySelector('[data-start]'),
@@ -31,19 +28,11 @@ const refs = {
   minutesEl: document.querySelector('[data-minutes]'),
   secondsEl: document.querySelector('[data-seconds]')
 }
-
-refs.startBtnEl.addEventListener('click', onStartBtnClick.bind(options));
-addDisabledAttribute();
-
-function inputBlock(){
-  refs.inputEl.disabled = true;
-}
-
-function addDisabledAttribute(){
-  refs.startBtnEl.setAttribute('disabled', '');
-}
-
+refs.startBtnEl.addEventListener('click', onStartBtnClick);
 function onStartBtnClick(){
+  if (!startTime) {
+    return;
+  }
   const intervalId = setInterval(() => {
     const currentTime = Date.now();
     const ms = startTime - currentTime;
@@ -57,14 +46,18 @@ function onStartBtnClick(){
   inputBlock();
   addDisabledAttribute();
 }
-
+function inputBlock(){
+  refs.inputEl.disabled = true;
+}
+function addDisabledAttribute(){
+  refs.startBtnEl.setAttribute('disabled', '');
+}
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
@@ -73,22 +66,117 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-  // console.log({days, hours, minutes, seconds})
   return { days, hours, minutes, seconds };
 }
-
 function addLeadingZero(time){
-const days = time.days.toString().padStart(2, 0);
-const hours = time.hours.toString().padStart(2, 0);
-const minutes = time.minutes.toString().padStart(2, 0);
-const seconds = time.seconds.toString().padStart(2, 0);
-return {days, hours, minutes, seconds};
+  const days = time.days.toString().padStart(2, 0);
+  const hours = time.hours.toString().padStart(2, 0);
+  const minutes = time.minutes.toString().padStart(2, 0);
+  const seconds = time.seconds.toString().padStart(2, 0);
+  return {days, hours, minutes, seconds};
 }
-
 function renderStopWatch({days, hours, minutes, seconds}){
   refs.daysEl.textContent = days;
   refs.hoursEl.textContent = hours;
   refs.minutesEl.textContent = minutes;
   refs.secondsEl.textContent = seconds;
 }
+
+
+
+
+
+
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+// import Notiflix from 'notiflix';
+// let startTime;
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     const selectTime = selectedDates[0] - options.defaultDate;
+//     if(selectTime < 0){
+//       // alert("Please choose a date in the future");
+//       Notiflix.Notify.failure("Please choose a date in the future");
+//       addDisabledAttribute();
+//       return;
+//     }
+//     refs.startBtnEl.removeAttribute('disabled');
+//     startTime = selectedDates[0].getTime();
+//   }
+// };
+
+// flatpickr("#datetime-picker", options);
+
+// const refs = {
+//   inputEl: document.getElementById('datetime-picker'),
+//   startBtnEl: document.querySelector('[data-start]'),
+//   daysEl: document.querySelector('[data-days]'),
+//   hoursEl: document.querySelector('[data-hours]'),
+//   minutesEl: document.querySelector('[data-minutes]'),
+//   secondsEl: document.querySelector('[data-seconds]')
+// }
+
+// refs.startBtnEl.addEventListener('click', onStartBtnClick.bind(options));
+// addDisabledAttribute();
+
+// function inputBlock(){
+//   refs.inputEl.disabled = true;
+// }
+
+// function addDisabledAttribute(){
+//   refs.startBtnEl.setAttribute('disabled', '');
+// }
+
+// function onStartBtnClick(){
+//   const intervalId = setInterval(() => {
+//     const currentTime = Date.now();
+//     const ms = startTime - currentTime;
+//     const time = convertMs(ms);
+//     const withZero = addLeadingZero(time);
+//     renderStopWatch(withZero);
+//     if(time.days === 0 && time.hours === 0 && time.minutes === 0 && time.seconds === 0){
+//       clearInterval(intervalId)
+//     }
+//   }, 1000);
+//   inputBlock();
+//   addDisabledAttribute();
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+//   // console.log({days, hours, minutes, seconds})
+//   return { days, hours, minutes, seconds };
+// }
+
+// function addLeadingZero(time){
+// const days = time.days.toString().padStart(2, 0);
+// const hours = time.hours.toString().padStart(2, 0);
+// const minutes = time.minutes.toString().padStart(2, 0);
+// const seconds = time.seconds.toString().padStart(2, 0);
+// return {days, hours, minutes, seconds};
+// }
+
+// function renderStopWatch({days, hours, minutes, seconds}){
+//   refs.daysEl.textContent = days;
+//   refs.hoursEl.textContent = hours;
+//   refs.minutesEl.textContent = minutes;
+//   refs.secondsEl.textContent = seconds;
+// }
